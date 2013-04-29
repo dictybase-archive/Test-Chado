172a4fe3747689c71a8c2741a94c7c69b5b155e2
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2013-04-29 13:04:24 -0500

    Added DbManger role

--- lib/Test/Chado/Types.pm
+++ lib/Test/Chado/Types.pm
@@ -2,9 +2,10 @@ package Test::Chado::Types;
 
 use Type::Library
     -base,
-    -declare => qw(DBH);
+    -declare => qw(DBH DbManager);
 use Type::Utils;
 
 class_type DBH, { class => "DBI::db"};
+role_type DbManager, { role => 'Test::Chado::Role::HasDBManager'};
 
 1;
