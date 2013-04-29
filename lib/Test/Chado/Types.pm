e1f1478c24cc1c3f8dea8d1ad67218b1600172f6
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2013-04-29 13:46:46 -0500

    exported standard types

--- lib/Test/Chado/Types.pm
+++ lib/Test/Chado/Types.pm
@@ -4,6 +4,7 @@ use Type::Library
     -base,
     -declare => qw(DBH DbManager);
 use Type::Utils;
+use Types::Standard -types;
 
 class_type DBH, { class => "DBI::db"};
 role_type DbManager, { role => 'Test::Chado::Role::HasDBManager'};
