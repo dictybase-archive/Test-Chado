eca7b0798a346876b7132990eb9b450971feab12
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2013-05-02 14:48:22 -0500

    Added few more class types

--- lib/Test/Chado/Types.pm
+++ lib/Test/Chado/Types.pm
@@ -2,10 +2,13 @@ package Test::Chado::Types;
 
 use Type::Library
     -base,
-    -declare => qw(DBH DbManager BCS HashiFied);
+    -declare => qw(DBH DbManager BCS Twig Graph GraphT HashiFied);
 use Type::Utils;
 
 class_type DBH, { class => "DBI::db"};
+class_type Twig, { class => "XML::Twig"};
+class_type Graph, { class => "Graph"};
+class_type GraphT, { class => "Graph::Traversal"};
 class_type BCS, { class => "Bio::Chado::Schema"};
 class_type HashiFied, { class => "Data::Perl::Collection::Hash"};
 role_type DbManager, { role => 'Test::Chado::Role::HasDBManager'};
