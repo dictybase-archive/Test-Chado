bbfc7c020d2d5198098746eb0e334d81c95f788f
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2013-06-03 14:31:29 -0500

    API and SYNOPSIS for Test::Chado

--- lib/Test/Chado/Types.pm
+++ lib/Test/Chado/Types.pm
@@ -25,3 +25,85 @@ declare MaybeFixtureLoader, as Maybe[FixtureLoader];
 declare MaybeDbManager,     as Maybe[DbManager];
 
 1;
+
+
+=head1 SYNOPSIS
+
+=over
+
+=item In the consuming class
+
+use Test::Chado::Types qw/BCS FixtureLoader/;
+use Moo;
+use Test::Chado::FixtureLoader::Preset;
+
+has 'schema' => ( is => 'rw', isa => BCS);
+
+has 'loader' => (
+    is => 'rw', 
+    isa => FixtureLoader,
+    lazy => 1,
+    default => sub { return Test::Chado::FixtureLoader::Preset->new }
+);
+
+
+=back
+
+
+=head1 Types defined
+
+=over
+
+=item DBH
+
+The L<DBI> connect object
+
+=item DbManager
+
+A consuming class that consumes L<Test::Chado::Role::HasDBManager>
+
+=item BCS
+
+L<Bio::Chado::Schema> object
+
+=item Twig
+
+L<XML::Twig> object
+
+=item Graph
+
+L<Graph> object
+
+=item GraphT
+
+L<Graph::Traversal> object
+
+=item HashiFied
+
+L<Data::Perl::Collection::Hash> object
+
+=item FixtureLoader
+
+Class that consumes L<Test::Chado::Role::Helper::WithBcs> role
+
+=item FixtureManager
+
+Class that consumes L<Test::Chado::FixtureManager::Flatfile>
+
+=item DBIC
+
+L<DBIx::Class::Schema> object
+
+=item TB
+
+L<Test::Tester::Delegate> object
+
+=item MaybeDbManager
+
+To define a type which might or might not hold a L<DbManager> type
+
+=item MaybeFixtureLoader
+
+To define a type which might or might not hold a L<FixtureManager> type
+
+=back
