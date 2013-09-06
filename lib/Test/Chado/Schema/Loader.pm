ead75e6a24abbcdc287f2110bd8312beef00f8ed
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2013-09-06 15:46:15 -0500

    Added custom relationships name to match with BCS

--- lib/Test/Chado/Schema/Loader.pm
+++ lib/Test/Chado/Schema/Loader.pm
@@ -3,6 +3,12 @@ package Test::Chado::Schema::Loader;
 use base qw/DBIx::Class::Schema::Loader/;
 
 __PACKAGE__->naming('current');
+__PACKAGE__->loader_options(
+    rel_name_map => {
+        'cvtermsynonym_cvterms' => 'cvtermsynonyms',
+        'cvtermprop_cvterms'    => 'cvtermprops'
+    }
+);
 
 1;
 
